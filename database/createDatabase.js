16da4b1abbfeca20566e3cd08ad3704a9e469746
Carlos Illobre <carlos.illobre@rocksaucestudios.com>
2018-04-10 18:04:33 -0300

    docker

--- database/createDatabase.js
+++ database/createDatabase.js
@@ -2,14 +2,16 @@ const mongoose = require('mongoose')
 const glob = require('glob')
 const path = require('path')
 
-const url = 'mongodb://ds231739.mlab.com:31739/educacionit'
+//const url = 'mongodb://ds231739.mlab.com:31739/educacionit'
+const url = 'mongodb://mongo:27017/educacionit'
+
 
 module.exports = ({ logger }) => {
 
-    mongoose.connect(url, {
+    mongoose.connect(url)/*, {
         user: 'educacionit',
         pass: 'educacionit',
-    })
+    })*/
 
     const db = glob.sync('./schemas/**/*.js', { cwd: __dirname })
     .map(filename => {
